c0851bc049b4ea7f2f9d01ff819e84272e72706b
R0mulus <jozef.balint@akademiasovy.sk>
2016-11-09 11:28:03 +0100

    Added length input in file1.cpp

--- test/file1.cpp
+++ test/file1.cpp
@@ -1,20 +1,21 @@
 #include<stdio.h>
 
 main(){
-	
+
 	int num,zvysok;
-	
+
 	printf("Enter number: ");
+	printf("Enter length: ");
 	scanf("%d",&num);
-	
+
 	do{
 		zvysok=num%2;
 		printf("%d",zvysok);
 		num=(num/2);
-		
-	}while(num!=0);
-	
 
-	
-	
+	}while(num!=0);
+
+
+
+
 }
